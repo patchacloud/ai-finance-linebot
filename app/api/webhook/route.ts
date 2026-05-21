683bd7be0174cb085cb1c20fb532f109039e7bda
patchacloud <patcha.cloud@gmail.com>
2026-05-21 08:46:36 +0000

    enable webhook

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,6 +1,16 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function POST(req: NextRequest) {
+  const body = await req.json()
+
+  console.log(body)
+
+  return NextResponse.json({
+    ok: true,
+  })
+}
+
+export async function GET() {
   return NextResponse.json({
     ok: true,
   })
